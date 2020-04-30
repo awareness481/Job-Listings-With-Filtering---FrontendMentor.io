eb5fb23721715751c94d0733da5f7e0530a39c72
Jesse Jafa <jesse.jafa@gmail.com>
2020-04-30 09:47:54 +0300

    refactored reducers, added new state keys

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -1,27 +1,65 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 import {
   REQUEST_JOB_LISTINGS,
-  RECEIVE_JOB_LISTINGS
-} from '../actions/index';
+  RECEIVE_JOB_LISTINGS,
+  SET_FILTERS,
+  FILTER_JOBS,
+} from "../actions/index";
 
-function data(state = [], action) {
-  switch(action.type) {
+function listings(state = [], action) {
+  switch (action.type) {
     case REQUEST_JOB_LISTINGS:
-      return {
-        ...state
-      }
+      return [...state];
     case RECEIVE_JOB_LISTINGS:
+      return [...state, ...action.jobs];
+    default:
+      return state;
+  }
+}
+
+function form(state = { filters: [] }, action) {
+  switch (action.type) {
+    case SET_FILTERS:
+      const newState = [...state.filters];
+
+      if (newState.indexOf(action.filter) !== -1) {
+        const index = newState.indexOf(action.filter);
+        newState.splice(index, 1);
+      } else {
+        newState.push(action.filter);
+      }
+
       return {
         ...state,
-        ...action.jobs
-      }
+        filters: newState,
+      };
+    case FILTER_JOBS:
+      let list = [...action.jobs];
+
+      list = list.filter((listing) => {
+        let pre = false;
+        if (listing.languages) {
+          listing.languages.map((lang) => {
+            if (state.filters.includes(lang)) {
+              pre = true;
+            }
+          });
+        }
+
+        return pre;
+      });
+
+      return {
+        ...state,
+        filteredListings: list,
+      };
     default:
       return state;
   }
 }
 
 const rootReducer = combineReducers({
-  listings: data
+  listings,
+  form,
 });
 export default rootReducer;
-
