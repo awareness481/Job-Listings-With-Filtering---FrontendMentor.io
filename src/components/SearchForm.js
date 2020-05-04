7f293e71e4e26cd12944591adaa55fb0a1cb3259
Jesse Jafa <jesse.jafa@gmail.com>
2020-05-04 05:38:50 +0300

    initial Searchform implementantion

---
+++ src/components/SearchForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchForm = () => {
+  return (
+    <form className='form w-4/5 my-0 mx-auto shadow h-20'>
+      <input type='text' className='form__input'></input>
+    </form>
+  )
+}
+
+export default SearchForm;
