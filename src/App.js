e24cea8997097deca466144e5f8c036bc8a95e7e
Jesse Jafa <jesse.jafa@gmail.com>
2020-05-04 05:38:00 +0300

    Added tailwind to project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadData } from "./app/actions/index";
 import JobListings from "./components/JobListings";
+import NavBar from './components/Navbar';
+import SearchForm from './components/SearchForm';
 
 function App({ dispatch }) {
   useEffect(() => {
@@ -10,7 +12,8 @@ function App({ dispatch }) {
 
   return (
     <div className="App">
-      <JobListings />
+      <NavBar />
+      <SearchForm />
     </div>
   );
 }
