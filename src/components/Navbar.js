e890a257e68963bb7cef6d93cfb7811ba501aaec
Jesse Jafa <jesse.jafa@gmail.com>
2020-05-04 05:38:20 +0300

    Initial navbar styling

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SvgBgHeaderDesktop from './svg/SvgBgHeaderDesktop';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar w-full'>
+      {/* <SvgBgHeaderDesktop /> */}
+    </nav>
+  )
+}
+
+export default Navbar;
